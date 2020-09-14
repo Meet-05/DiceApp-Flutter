6ef9fb7c09d6272f92d42e3ec9852e73c1d8cec8
Meet-05 <meetpanchal9@gmail.com>
2020-09-14 19:19:55 +0530

    added state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(MaterialApp(
@@ -12,31 +13,67 @@ void main() {
   )));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          //the expanded widget is used inside a row or column and takes the full main axis space availabe
-          flex: 2,
-          child: FlatButton(
-            child: Image.asset('images/dice1.png'),
-            onPressed: () {
-              print('left button pressed');
-            },
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: <Widget>[
+              Expanded(
+                //the expanded widget is used inside a row or column and takes the full main axis space availabe
+                child: FlatButton(
+                  onPressed: () {
+                    changeDice();
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                //the expanded widget is used inside a row or column and takes the full main axis space availabe
+                child: FlatButton(
+                  onPressed: () {
+                    setState(
+                      () {
+                        changeDice();
+                      },
+                    );
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ],
           ),
-        ),
-        Expanded(
-          //the expanded widget is used inside a row or column and takes the full main axis space availabe
-          child: FlatButton(
-            child: Image.asset('images/dice1.png'),
-            onPressed: () {
-              print('right Button Pressed');
-            },
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: RaisedButton(
+                onPressed: () {
+                  changeDice();
+                },
+                child: Text(
+                  'Roll',
+                  style: TextStyle(fontSize: 25),
+                )),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
